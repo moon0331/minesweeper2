5b850340a2e0c8d9ff9a5dc59ab6df801081220e
moon0331 <moon0331@skku.edu>
2021-11-05 00:46:30 +0900

    board.py 테스트

--- src/package/board/block.py
+++ src/package/board/block.py
@@ -1,7 +1,8 @@
 '''block definition'''
 
 class Block:
-    def __init__(self, has_bomb=False, n_adj_bomb=0):
+    def __init__(self, loc, has_bomb=False, n_adj_bomb=0):
+        self.loc = loc
         self.has_bomb = has_bomb
         self.n_adj_bomb = n_adj_bomb
         self.image = self._generate_image(has_bomb)
@@ -12,7 +13,18 @@ class Block:
         else:
             self._set_n_adj_bomb(n_adj_bomb)
 
-    def _generate_image(self):
+    def _set_bomb(self):
+        self.has_bomb = True
+
+    def _generate_image(self, has_bomb):
         if self.has_bomb:
-            image = None # get bomb image file
+            image = 'Bomb.png' # get bomb image file
+        else:
+            image = ''
         return image
+
+    def __repr__(self):
+        if self.has_bomb:
+            return f'[({self.loc[0]},{self.loc[1]}), BOMB, {self.n_adj_bomb}]'
+        else:
+            return f'[({self.loc[0]},{self.loc[1]}), ----, {self.n_adj_bomb}]'
--- src/package/board/board.py
+++ src/package/board/board.py
@@ -1,5 +1,7 @@
 from random import randint
-from package.board.board import Block
+# from package.board.board import Block
+
+from block import Block
 
 '''board definition'''
 
@@ -10,28 +12,49 @@ class Board:
         중급: 16×16 넓이의 지뢰밭에 40개의 지뢰
         고급: 30×16 넓이의 지뢰밭에 99개의 지뢰
         '''
-        self.board_size = size
+        self.size = size
         self.n_bomb = n_bomb
         self.remain_bomb = n_bomb # log
+
+        self.bomb_loc = set()
+        self.board = None
         
-        self.bomb_loc = self._randomize_bomb() # 지뢰 위치 랜덤 결정
-        self.board = self._generate() # 보드 생성
+        self._randomize_bomb() # 지뢰 위치 랜덤 결정
+        self._generate() # 보드 생성
+
         self._calculate_bomb_distance() # 지뢰 개수 세어 저장
     
     def _randomize_bomb(self):
-        rnd_loc = {}
-        while len(rnd_loc) < self.n_bomb:
+        self.bomb_loc = set()
+        while len(self.bomb_loc) < self.n_bomb:
             x = randint(0, self.size-1)
             y = randint(0, self.size-1)
-            rnd_loc.add((x, y))
+            self.bomb_loc.add((x, y))
 
 
     def _generate(self):
-        pass
+        size = self.size
+        self.board = [[Block((row, col)) for col in range(size)] for row in range(size)]
+        for r, c in self.bomb_loc:
+            self.board[r][c]._set_bomb()
 
     def _calculate_bomb_distance(self):
-        pass
+        size = self.size
+        bomb_distance = [[0 for _ in range(size)] for _ in range(size)]
+        for r, c in self.bomb_loc:
+            for adj_r in range(r-1, r+2):
+                for adj_c in range(c-1, c+2):
+                    if not (r == adj_r == adj_c) and 0<=adj_r<size and 0<=adj_c<size:
+                        bomb_distance[adj_r][adj_c] += 1
 
-    def print_board(self, law = False, flatten=False, add_bomb_loc=False):
+        for r in range(size):
+            for c in range(size):
+                self.board[r][c].n_adj_bomb = bomb_distance[r][c]
+
+
+    def print_board(self, raw = False, flatten=False, add_bomb_loc=False):
         for line in self.board:
-            print(" ".join(line))
+            print(" | ".join([str(x) for x in line]))
+
+if __name__ == '__main__':
+    Board(4,3).print_board()
