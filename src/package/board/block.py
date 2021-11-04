add97f4b4443c1c58db12df8e13e3f2e7a8e6981
moon0331 <moon0331@skku.edu>
2021-11-05 00:53:43 +0900

    board 수정

--- src/package/board/block.py
+++ src/package/board/block.py
@@ -1,30 +1,35 @@
 '''block definition'''
 
 class Block:
-    def __init__(self, loc, has_bomb=False, n_adj_bomb=0):
+    def __init__(self, loc, has_bomb=False):
         self.loc = loc
         self.has_bomb = has_bomb
-        self.n_adj_bomb = n_adj_bomb
-        self.image = self._generate_image(has_bomb)
+        self.n_adj_bomb = 0
 
-    def _set_atrribute(self, n_adj_bomb=0):
+        self.image = ''
+        self._generate_image(has_bomb)
+
+    def click(self):
         if self.has_bomb:
-            self._set_bomb()
+            return True # 폭탄 있음 -> 후처리 필요
         else:
-            self._set_n_adj_bomb(n_adj_bomb)
+            return False # 폭탄 없음 -> 연쇄 폭발 구현 필요
 
     def _set_bomb(self):
         self.has_bomb = True
 
     def _generate_image(self, has_bomb):
         if self.has_bomb:
-            image = 'Bomb.png' # get bomb image file
+            self.image = 'Bomb.png' # get bomb image file
         else:
-            image = ''
-        return image
+            self.image = ''
 
     def __repr__(self):
         if self.has_bomb:
             return f'[({self.loc[0]},{self.loc[1]}), BOMB, {self.n_adj_bomb}]'
         else:
             return f'[({self.loc[0]},{self.loc[1]}), ----, {self.n_adj_bomb}]'
+
+if __name__ == '__main__':
+    print(Block((13,2)))
+    print(Block((12,3, True)))
